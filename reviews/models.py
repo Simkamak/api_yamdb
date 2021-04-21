11155ddf2df88ea5ad7e75c9f2c806b1e92b0a6c
bitcoin <bitcoineazy@gmail.com>
2021-04-21 13:32:14 +0300

    Review & Comment models admin panel - added

--- reviews/models.py
+++ reviews/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-
 User = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField('Название категории', max_length=200)
+    slug = models.SlugField(unique=True)
+
+
+class Title(models.Model):
+    name = models.CharField('Название произведения', max_length=200)
+    year = models.IntegerField('Год выхода произведения')
+    category = models.ForeignKey(Category, related_name='titles',
+                                 on_delete=models.CASCADE)
 
 
 class Review(models.Model):
@@ -11,7 +22,7 @@ class Review(models.Model):
     text = models.TextField('Отзыв')
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='reviews')
-    score = models.IntegerField(choices=(i, i) for i in range(0, 10))
+    score = models.IntegerField(choices=((i, i) for i in range(1, 11)))
     pub_date = models.DateTimeField('дата отзыва', auto_now_add=True)
 
 
