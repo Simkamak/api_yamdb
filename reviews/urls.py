6e9da7b57da3d2d4930548d41a79d0ac0dc628ea
Matvey <bitcoineazy@gmail.com>
2021-04-22 11:12:23 +0300

    ReviewViewSet get request without pagination - added

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'titles/(?P<title_id>\d+)/reviews', views.ReviewViewSet, basename='reviews')
+
 
 urlpatterns = [
+    path('', include(router.urls))
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,2 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import viewsets
+from rest_framework.permissions import (IsAuthenticatedOrReadOnly)
 
+from .models import Review, Comment, Title
+from .serializers import ReviewSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = (IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly)
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, id=self.kwargs['title_id'])
+        return title.reviews.all()
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    permission_classes = (IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly)
