246e45c952821f08999eb8bda990259dda87f791
Simkamak <simka.mak87@mail.ru>
2021-04-23 13:13:39 +0300

    correct auth

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router_v1 = DefaultRouter()
+
+router_v1.register('v1/users/', views.CustomUserViewSet)
 
 urlpatterns = [
-    path('auth/email/', views.get_confirmation_code),
-    path('auth/token/', views.get_token),
-   # path('users/me/', )
+    path('', include(router_v1.urls)),
+    path('v1/auth/email/', views.get_confirmation_code),
+    path('v1/auth/token/', views.get_token),
+
 ]
--- users/views.py
+++ users/views.py
@@ -2,57 +2,60 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from rest_framework import status
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, get_list_or_404
+from rest_framework import permissions, viewsets
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
-import uuid
-from django.core.mail import send_mail
-from .serializers import RegistrationSerializer
+from django.core.mail import send_mail, BadHeaderError
+from .serializers import CustomUserSerializer
 from rest_framework_simplejwt.tokens import AccessToken
 
 User = get_user_model()
 
 
 @api_view(['POST'])
-#@permission_classes(AllowAny)
+
 def get_confirmation_code(request):
     email = request.data['email']
-    confirmation_code = uuid.uuid3(uuid.NAMESPACE_DNS, email)
-    updated, created = User.objects.update_or_create(username=email, email=email, defaults={'password': confirmation_code})
-    if created:
-        send_mail('Confirmation', f'Your code: {confirmation_code}',
-                      'admin@admin.ru', [request.data['email']])
-        data = {
-            'email': email,
-            'confirmation_code': confirmation_code
-                }
-
-        serializer = RegistrationSerializer(
-                data=data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    if User.objects.filter(email=email).exists():
+        return Response(f'User with email: {email} already exists')
     else:
-        return Response(f'You are already signed in', status=status.HTTP_200_OK)
+        confirmation_code = User.objects.make_random_password()
+        User.objects.create_user(username=request.data['email'], email=email,
+                                 confirmation_code=confirmation_code)
+
+        try:
+            send_mail('Confirmation', f'Your code: {confirmation_code}',
+                  'admin@admin.ru', [email])
+            return Response({'email': confirmation_code},
+                            status=status.HTTP_200_OK)
+        except BadHeaderError:
+            return Response({'errors': 'incorrect e-mail {}'.format(email)},
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
-#@permission_classes(AllowAny)
+
 def get_token(request):
     try:
         email = request.data['email']
         confirmation_code = request.data['confirmation_code']
         user = get_object_or_404(User, email=email,
                                  confirmation_code=confirmation_code)
-        refresh = AccessToken.for_user(user)
+        token = AccessToken.for_user(user)
 
-        return Response({'access': str(refresh)}, status=status.HTTP_200_OK)
-    except:
-        return Response(status.HTTP_400_BAD_REQUEST)
+        return Response({'token': str(token)}, status=status.HTTP_200_OK)
+    except BadHeaderError:
+        return Response({}, status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
-def api_users(request):
-    users = User.objects.all()
-    role_admin = request.user.has_perm('admin')
-    serializer = RegistrationSerializer(users, many=True)
+
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = CustomUserSerializer
+    #permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+
+
 
