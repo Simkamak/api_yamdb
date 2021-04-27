ba41417313c264dd64d89ecb6a35ae223a8ce42e
Simkamak <simka.mak87@mail.ru>
2021-04-27 10:14:41 +0300

    add Moderator, AuthUser, AnonymousReadOnly permissions

--- users/permissions.py
+++ users/permissions.py
@@ -23,3 +23,32 @@ class ReadOnly(permissions.BasePermission):
             return True
 
 
+class AnonymousReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+
+class AuthUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            return (
+                    request.user.is_authenticated
+                    and request.user.role == User.UserRole.USER
+            )
+
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user
+
+
+class Moderator(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            return (
+                    request.user.is_authenticated
+                    and request.user.role == User.UserRole.MODERATOR
+            )
