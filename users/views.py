4c839f851a8c486f27db833a4751222eb61d845d
Matvey <bitcoineazy@gmail.com>
2021-05-01 23:45:31 +0300

    ReviewApp - normalized

--- users/views.py
+++ users/views.py
@@ -1,25 +1,56 @@
-from rest_framework import filters, permissions, status, viewsets
-from rest_framework.decorators import action
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions, status
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import AccessToken
 
-from .models import User
-from .permissions import IsYAMDBAdministrator
-from .serializers import UserSerializer
+from api_yamdb.settings import SERVER_EMAIL
+
+User = get_user_model()
 
 
-class UserList(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    lookup_field = 'username'
-    permission_classes = [IsYAMDBAdministrator]
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['username', ]
+@api_view(['POST'])
+@permission_classes([permissions.AllowAny])
+def get_confirmation_code(request):
+    try:
+        email = request.data['email']
+        username = request.data['username']
+        user = get_object_or_404(User, email=email, username=username)
+        confirmation_code = User.objects.make_random_password()
+        user.confirmation_code = confirmation_code
+        user.save()
+        send_mail('Confirmation', f'Your code: {confirmation_code}',
+                  SERVER_EMAIL, [email])
+        return Response({'email': confirmation_code, 'username': username},
+                        status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({'errors': str(e)},
+                        status=status.HTTP_400_BAD_REQUEST)
 
-    @action(detail=False, methods=['get', 'put', 'patch'], url_path='me',
-            permission_classes=[permissions.IsAuthenticated])
-    def me(self, request):
-        user = self.request.user
-        serializer = self.get_serializer(user, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@permission_classes([permissions.AllowAny])
+def get_token(request):
+    try:
+        email = request.data['email']
+        confirmation_code = request.data['confirmation_code']
+        if confirmation_code != '':
+            user = get_object_or_404(
+                User, email=email, confirmation_code=confirmation_code
+                )
+            token = AccessToken.for_user(user)
+            user.confirmation_code = ''
+            user.save()
+            return Response({'token': str(token)}, status=status.HTTP_200_OK)
+        else:
+            return Response(
+                {'error': 'Confirmation_code  is required.'},
+                status.HTTP_203_NON_AUTHORITATIVE_INFORMATION
+            )
+
+    except Exception as e:
+        return Response(
+            {'error': str(e)}, status.HTTP_203_NON_AUTHORITATIVE_INFORMATION
+        )
